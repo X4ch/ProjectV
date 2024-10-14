b09756e361fb4b2c31d10cdb1ad603373d8c56a5
X4ch <t.moilleron@gmail.com>
2024-10-14 18:08:48 +0200

    Changed the way teleporter works

--- Assets/Scripts/Teleporter.cs
+++ Assets/Scripts/Teleporter.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 
 public class Teleporter : MonoBehaviour
 {
-
+    [SerializeField] GameObject carPrefab;
     [SerializeField] GameObject exitPortal;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        GameObject carObject = collision.gameObject;
         CarController car = collision.GetComponent<CarController>();
         if (car != null && car.canTeleport)
         {
-            collision.transform.position = exitPortal.transform.position;
-            car.setTeleportationTime(0);
-            car.canTeleport = false;
+            Destroy(carObject);
+            GameObject carInstance = Instantiate(carPrefab, exitPortal.transform.position, exitPortal.transform.rotation);
+            carInstance.GetComponent<CarController>().setTeleportationTime(0);
+            carInstance.GetComponent<CarController>().canTeleport = false;
         }
     }
 }
