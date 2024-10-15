e28df0edd9e7a1371baa144bbeaf077472a402e8
Vipixx1 <lyvictor1@gmail.com>
2024-10-15 15:25:46 +0200

    Fixed teleportation

--- Assets/Scripts/Teleporter.cs
+++ Assets/Scripts/Teleporter.cs
@@ -12,13 +12,23 @@ public class Teleporter : MonoBehaviour
     {
         GameObject carObject = collision.gameObject;
         CarController car = collision.GetComponent<CarController>();
+
         if (car != null)
         {
-            //Destroy(carObject);
-            //Instantiate(carPrefab, exitPortal.transform.position, exitPortal.transform.rotation);
-
             carObject.transform.position = exitPortal.transform.position;
+
+            Rigidbody2D carRigidbody2D = carObject.GetComponent<Rigidbody2D>();
+            Vector2 currentVelocity = carRigidbody2D.velocity;
+
+            Vector2 portalForwardDirection = exitPortal.transform.up;
+
+            float currentSpeed = currentVelocity.magnitude;
+            Vector2 newVelocity = portalForwardDirection * currentSpeed;
+
+            carRigidbody2D.velocity = newVelocity;
+
             car.setRotation(exitPortal.transform.rotation.eulerAngles.z);
         }
     }
+
 }
